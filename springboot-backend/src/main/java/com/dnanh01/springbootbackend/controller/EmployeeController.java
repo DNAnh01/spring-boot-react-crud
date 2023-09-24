a95506f44d235a285cd4102a4cb87c628bcf9c2d
DNAnh01 <donguyenanhgithub@gmail.com>
2023-09-25 01:18:30 +0700

    #9 - Connecting React with List

--- springboot-backend/src/main/java/com/dnanh01/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/dnanh01/springbootbackend/controller/EmployeeController.java
@@ -4,6 +4,7 @@ package com.dnanh01.springbootbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dnanh01.springbootbackend.model.Employee;
 import com.dnanh01.springbootbackend.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
