ce96f065210a5ea55377be4013112faad09412e7
DNAnh01 <donguyenanhgithub@gmail.com>
2023-09-24 21:24:46 +0700

    #2 - Creating JPA Entity + Repository #3 - Creating List Employee REST API

---
+++ springboot-backend/src/main/java/com/dnanh01/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.dnanh01.springbootbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dnanh01.springbootbackend.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+    
+}
