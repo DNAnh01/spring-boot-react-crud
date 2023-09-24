6505096c53135e1ffe05436ffe2877f0b8873122
DNAnh01 <donguyenanhgithub@gmail.com>
2023-09-25 02:05:06 +0700

    #11 - Configure Routing

--- react-fontend/src/App.js
+++ react-fontend/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
     return (
-        <div className="container">
-            <HeaderComponent />
-            <div>
-                <ListEmployeeComponent />
+        <Router>
+            <div className="container">
+                <HeaderComponent />
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<ListEmployeeComponent />} />
+                        <Route path="/employees" element={<ListEmployeeComponent />} />
+                    </Routes>
+                </div>
+                <FooterComponent />
             </div>
-            <FooterComponent />
-        </div>
+        </Router>
     );
 }
 
