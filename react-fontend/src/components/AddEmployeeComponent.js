d614e213589b7d8779095c0ef30fb0417d948e9d
DNAnh01 <donguyenanhgithub@gmail.com>
2023-09-28 05:53:58 +0700

    #15 - Build Get Employee By Id REST API

--- react-fontend/src/components/AddEmployeeComponent.js
+++ react-fontend/src/components/AddEmployeeComponent.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import EmployeeService from '../sevices/EmployeeService';
+import EmployeeService from '../services/EmployeeService';
 
 const AddEmployeeComponent = () => {
     const [firstName, setFirstName] = useState('');
--- springboot-backend/src/main/java/com/dnanh01/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/dnanh01/springbootbackend/controller/EmployeeController.java
@@ -4,19 +4,22 @@ package com.dnanh01.springbootbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dnanh01.springbootbackend.exception.ResourceNotFoundException;
 import com.dnanh01.springbootbackend.model.Employee;
 import com.dnanh01.springbootbackend.repository.EmployeeRepository;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/employees/")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -31,5 +34,12 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    // Get employee by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id" + id));
+        return ResponseEntity.ok(employee);
     }
 }
