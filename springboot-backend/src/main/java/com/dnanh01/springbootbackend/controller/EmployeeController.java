b69f886f2dbafd8ef6247a5df048045349ba7f48
DNAnh01 <donguyenanhgithub@gmail.com>
2023-09-28 07:06:52 +0700

    #19 - Build Delete Employee REST API

--- springboot-backend/src/main/java/com/dnanh01/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/dnanh01/springbootbackend/controller/EmployeeController.java
@@ -4,8 +4,10 @@ package com.dnanh01.springbootbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,5 +58,14 @@ public class EmployeeController {
         employeeRepository.save(updateEmployee);
         return ResponseEntity.ok(updateEmployee);
 
+    }
+
+    // delete employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id) {
+        Employee employee = employeeRepository.findById(id)
+                                            .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id" + id));
+        employeeRepository.delete(employee);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
